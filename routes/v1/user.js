b98401c6d81c1566178828bbd7b538527da72449
Thinh Ha <havanthinh97@gmail.com>
2022-07-05 18:59:59 +0700

    add verify route

--- routes/v1/user.js
+++ routes/v1/user.js
@@ -56,4 +56,22 @@ router.post('/signin', async (req, res) => {
   }
 })
 
+router.post('/verify/:id', async (req, res) => {
+  try {
+    const { id } = req.params
+    const user = await User.findById(id)
+    if (user === null) {
+      res.status(404).json({message: 'User not found'})
+    } else {
+      user.verified = true
+      await user.save()
+      res.status(200).json({message: 'User has been successfully verified'})
+    }
+  } catch (err) {
+    console.error('Error at /api/v1/user/verify/:id')
+    console.error(err)
+    res.status(500).json({ message: 'Internal Server Error', err: `${err}` })
+  }
+})
+
 module.exports = router
