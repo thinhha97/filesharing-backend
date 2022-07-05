99d5e05a09f8e2b7b2514bd925058c3fa6bcaba1
Thinh Ha <havanthinh97@gmail.com>
2022-07-05 18:08:59 +0700

    create signup route.

--- routes/v1/user.js
+++ routes/v1/user.js
@@ -1,3 +1,30 @@
 const router = require('express').Router()
+const bcrypt = require('bcrypt')
+const User = require('$models/user')
+
+router.post('/signup', async (req, res) => {
+  try {
+    const { email, password } = req.body
+    const user = await User.findOne({ email })
+    if (user !== null) {
+      res.status(409).json({
+        message: 'An user is already existed with provided email address.',
+      })
+    } else {
+      const salt = await bcrypt.genSalt(12)
+      const hashedPassword = await bcrypt.hash(password, salt)
+      const newUser = new User({
+        email,
+        passwords: [hashedPassword],
+      })
+      await newUser.save()
+      console.log(newUser)
+      res.status(201).json({ id: newUser._id, email })
+      
+    }
+  } catch (err) {
+    res.status(500).json({ message: 'Internal Server Error', err: `${err}` })
+  }
+})
 
 module.exports = router
