66ab0fa337a730379451f60d734c8cd8fdc9b553
Thinh Ha <havanthinh97@gmail.com>
2022-07-05 17:38:09 +0700

    connect to mongoose on app startup

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 require('dotenv').config()
 require('module-alias/register')
+require('$dtoMongoose/connect')()
 const server = require('./server')
 
 const PORT = process.env.PORT || 5000
-
 server.listen(PORT, () => {
   console.log(`Server is listening on port ${PORT}`)
 })
