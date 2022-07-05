c6d940f742d045ca0881e713592f784f8dd426ec
Thinh Ha <havanthinh97@gmail.com>
2022-07-05 16:59:45 +0700

    use module-alias requires

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 const express = require('express')
 const cookieParser = require('cookie-parser')
 
+const routes = require('$routes')
 const app = express()
 
 app.use(express.urlencoded({extended: true}))
 app.use(express.json())
 app.use(cookieParser())
+
+app.use(routes)
 
 module.exports = app
