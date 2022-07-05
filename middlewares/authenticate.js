7c2f81f98c6093e3944aef923af76fef64774ae7
Thinh Ha <havanthinh97@gmail.com>
2022-07-05 20:38:41 +0700

    create authenticate middleware

---
+++ middlewares/authenticate.js
@@ -0,0 +1,30 @@
+const jwt = require('jsonwebtoken')
+const User = require('$models/user')
+const JWT_SECRET = process.env.JWT_SECRET
+
+module.exports = (req, res, next) => {
+  try {
+    const authHeader = req.headers['authorization']
+    const token = authHeader && authHeader.split(' ')[1]
+    if (token === null) {
+      res.status(401).json({ message: 'Access token required' })
+    } else {
+      jwt.verify(token, JWT_SECRET, async (err, data) => {
+        if (err) {
+          console.error(err)
+        }
+        const user = await User.findById(id)
+        if (user === null) {
+          res.status(401).json({ message: 'Invalid token provided' })
+        } else {
+          req.userId = user._id
+          next()
+        }
+      })
+    }
+  } catch (err) {
+    console.error('Error authenticating in "middlewares/authenticate.js"')
+    console.error(err)
+    res.status(500).json({ message: 'Internal Server Error', err: `${err}` })
+  }
+}
