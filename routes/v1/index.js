452b65c70ec273fabf27368ccf3937a3dbb03b95
Thinh Ha <havanthinh97@gmail.com>
2022-07-05 19:47:10 +0700

    get /api/v1 return 'hi'

--- routes/v1/index.js
+++ routes/v1/index.js
@@ -1,5 +1,9 @@
 const router = require('express').Router()
 
+router.get('/', (req, res) => {
+  res.status(200).json({message: 'hi'})
+})
+
 router.use('/user', require('./user'))
 
 module.exports = router
