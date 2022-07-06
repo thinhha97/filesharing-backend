b738d62d30e224108af66e3709a3da4b175f2cba
Thinh Ha <havanthinh97@gmail.com>
2022-07-06 16:06:37 +0700

    refactor token schema

--- models/token.js
+++ models/token.js
@@ -1,22 +1,24 @@
 const mongoose = require('mongoose')
 
-const tokenSchema = new mongoose.Schema(
-  {
-    userId: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: 'User',
-      required: true,
-    },
-    token: {
-      type: String,
-      required: true,
-    },
-    expiredAt: {
-      type: Date,
-      expires: 900,
-    },
-  },
-  { timestamps: true }
+const PASSWORD_RESET_TOKEN_EXPIRACY_DURATION = parseInt(
+  process.env.PASSWORD_RESET_TOKEN_EXPIRACY_DURATION
 )
+
+const tokenSchema = new mongoose.Schema({
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true,
+  },
+  token: {
+    type: String,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+    expires: PASSWORD_RESET_TOKEN_EXPIRACY_DURATION,
+  },
+})
 
 module.exports = mongoose.model('Token', tokenSchema)
