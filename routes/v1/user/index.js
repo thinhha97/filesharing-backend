676da08f3e5b321a529b1ca8c74bd0003be66a37
Thinh Ha <havanthinh97@gmail.com>
2022-07-06 10:57:48 +0700

    add checks for required fields.

--- routes/v1/user/index.js
+++ routes/v1/user/index.js
@@ -9,6 +9,11 @@ const BCRYPT_SALT = parseInt(process.env.BCRYPT_SALT)
 router.post('/signup', async (req, res) => {
   try {
     const { email, password } = req.body
+    if (!email || !password) {
+      return res
+        .status(400)
+        .json({ message: 'Missing one or more required field(s).' })
+    }
     const user = await User.findOne({ email })
     if (user === null) {
       const salt = await bcrypt.genSalt(BCRYPT_SALT)
@@ -19,7 +24,9 @@ router.post('/signup', async (req, res) => {
       })
       await newUser.save()
       // TODO: email verification link to user's email.
-      res.status(201).json({message: 'Account created successfully', email })
+      return res
+        .status(201)
+        .json({ message: 'Account created successfully', email })
     } else {
       res.status(409).json({
         message: 'An user is already existed with provided email address.',
@@ -28,52 +35,65 @@ router.post('/signup', async (req, res) => {
   } catch (err) {
     console.error('Error at /api/v1/user/signup')
     console.error(err)
-    res.status(500).json({ message: 'Internal Server Error', err: `${err}` })
+    return res
+      .status(500)
+      .json({ message: 'Internal Server Error', err: `${err}` })
   }
 })
 
 router.post('/signin', async (req, res) => {
   try {
     const { email, password } = req.body
+    if (!email || !password) {
+      return res
+        .status(400)
+        .json({ message: 'Missing one or more required field(s).' })
+    }
     const user = await User.findOne({ email })
 
     if (user === null) {
-      res
+      return res
         .status(404)
         .json({ message: 'An user with provided email not found.' })
     } else {
       const matched = await bcrypt.compare(password, user.passwords[0])
       if (!matched) {
-        res.status(401).json({ message: 'Invalid password' })
+        return res.status(401).json({ message: 'Invalid password' })
       } else {
         const token = jwt.sign(user._id.toString(), JWT_SECRET)
         user.lastSignin = new Date()
         await user.save()
-        res.status(200).json({ message: 'Successfully signing in', token })
+        return res.status(200).json({ message: 'Successfully signing in', token })
       }
     }
   } catch (err) {
     console.error('Error at /api/v1/user/signin')
     console.error(err)
-    res.status(500).json({ message: 'Internal Server Error', err: `${err}` })
+    return res.status(500).json({ message: 'Internal Server Error', err: `${err}` })
   }
 })
 
 router.post('/verify/:id', async (req, res) => {
   try {
     const { id } = req.params
+    if (!id) {
+      return res
+        .status(400)
+        .json({ message: 'Missing one or more required parameter(s).' })
+    }
+
     const user = await User.findById(id)
     if (user === null) {
-      res.status(404).json({message: 'User not found'})
+      return res.status(404).json({ message: 'User not found' })
     } else {
       user.verified = true
       await user.save()
-      res.status(200).json({message: 'User has been successfully verified'})
+      return res.status(200).json({ message: 'User has been successfully verified' })
     }
   } catch (err) {
     console.error('Error at /api/v1/user/verify/:id')
     console.error(err)
-    res.status(500).json({ message: 'Internal Server Error', err: `${err}` })
+    return res.status(500).json({ message: 'Internal Server Error', err: `${err}` })
   }
 })
 
